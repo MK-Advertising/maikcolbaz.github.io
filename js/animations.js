b710bb0fa22bd5ea7b670d1accfadbdce99e1d98
MK-Advertising <maikcolbaz@gmail.com>
2026-06-24 03:34:42 -0600

    Add GSAP animations for reveal and hero video

--- js/animations.js
+++ js/animations.js
@@ -1,1 +1,15 @@
+gsap.registerPlugin(ScrollTrigger);
 
+gsap.to('.reveal', {
+    opacity: 1,
+    y: 0,
+    duration: 1,
+    stagger: 0.15,
+    ease: 'power3.out'
+});
+
+gsap.from('.hero-video video', {
+    scale: 1.08,
+    duration: 2,
+    ease: 'power2.out'
+});
